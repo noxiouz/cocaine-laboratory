741c83ff2567876fa8b1b3fabb49f63a198cf38a
Anton Tyurin <noxiouz@yandex.ru>
2014-04-25 16:18:41 +0400

    Remove ua

--- app.py
+++ app.py
@@ -2,18 +2,12 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 
-from cocaine.services import Service
-
-uatraits = Service("uatraits")
-
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    ua = request.headers.get('User-Agent')
-    res = uatraits.detect(ua).get()
-    return jsonify(res)
+    return jsonify({"Message": "Hello world!"})
 
 if __name__ == "__main__":
     app.run(debug=True)
