6d6ae6e573f5d4194caa69f248480a6d847f07b8
Anton Tyurin <noxiouz@yandex.ru>
2014-04-26 12:21:34 +0400

    Step2

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return jsonify({"Message": "Hello world!"})
+    return jsonify({"Message": "Hello world! Version2"})
 
 if __name__ == "__main__":
     app.run(debug=True)
